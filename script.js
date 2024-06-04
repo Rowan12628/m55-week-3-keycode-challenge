cddf4525c18c169f9143b4b1fac60c58f8350023
Rowan12628 <170511926+Rowan12628@users.noreply.github.com>
2024-06-04 14:45:33 +0100

    add basic reporting of event properties

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Hello, world");
+
+const mainBody = document.getElementById("mainBody");
+
+document.addEventListener("keypress", (event) => {
+  console.log(event.key);
+  console.log(event.which);
+  console.log(event.code);
+
+  mainBody.innerHTML = ` event key = ${event.key}`;
+  mainBody.innerHTML += ` event which = ${event.which}`;
+  mainBody.innerHTML += ` event code = ${event.code}`;
+});
